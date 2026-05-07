84679b1c9e2e8824ea03203751bee52694c2f65b
CadyDB <40341@ma-web.nl>
2026-05-07 14:43:40 +0200

    les 1

---
+++ Assets/Les 1 stuffs/Scripts/Les1.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Les1 : MonoBehaviour
+{
+    [SerializeField] Tilemap myTilemap;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Debug.Log(mousePosition);
+
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            worldPosition.z = 0;
+            Debug.Log(worldPosition);
+
+            Vector3Int cellPosition = myTilemap.WorldToCell(worldPosition);
+            Debug.Log(cellPosition);
+        }
+    }
+}
