aa7094f97ab6735345388b454a5c890770ba464f
CadyDB <40341@ma-web.nl>
2026-06-22 10:03:38 +0200

    ah yes progress

--- Assets/GDV Kim/Match.cs
+++ Assets/GDV Kim/Match.cs
@@ -1,17 +1,26 @@
 using System.Data;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using TMPro;
+using static UnityEngine.Color;
+using UnityEngine.SceneManagement;
 
 public class Match : MonoBehaviour
 {
     [SerializeField] Tilemap myTilemap;
+    public static int score = 0;
+    public TextMeshProUGUI scoreText;
+    private AudioSource audioSource;
+    public AudioClip clickClip;
+    public AudioClip winClip;
     
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
     void Update()
     {
+        scoreText.text = Match.score.ToString();
         if (Input.GetMouseButtonDown(0))
         {
             //Debug.Log("boop");
@@ -27,10 +36,31 @@ public class Match : MonoBehaviour
             //Debug.Log(cellPosition);
 
             myTilemap.SetTileFlags(cellPosition, TileFlags.None);
-            myTilemap.SetColor(cellPosition, Color.lightCyan);
+            myTilemap.SetColor(cellPosition, Random.ColorHSV());
+            score += 2;
+            PlaySFX(clickClip);
+            
+            if(score == 100)
+            {
+                PlaySFX(winClip);
 
-            // als 2 tiles met dezelfde of nummers die 10 maken worden ingedrukt -> nummers verwijderen + 10 score, hoe?
+            }
+            if (score == 500)
+            {
+                PlaySFX(winClip);
+            }
+            if (score == 1000)
+            {
+                PlaySFX(winClip);
+            }
+            
         }
 
+    }
+    public void PlaySFX(AudioClip audioClip, float volume = 1f)
+    {
+        audioSource.clip = audioClip;
+        audioSource.volume = volume;
+        audioSource.Play();
     }
 }
